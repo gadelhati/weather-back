e5a11fcb95f17dc59e5601fa7f675635304a393f
GadelhaTI <gadelha.ti@gmail.com>
2023-05-21 00:02:22 -0300

    config security

--- src/main/java/com/observation/configuration/ConfigurationSecurity.java
+++ src/main/java/com/observation/configuration/ConfigurationSecurity.java
@@ -1,15 +1,17 @@
-package br.eti.observation.configuration;
+package com.observation.configuration;
 
-import br.eti.observation.security.JWTAuthEntryPoint;
-import br.eti.observation.security.JWTAuthenticationFilter;
-import br.eti.observation.service.ServiceCustomUserDetails;
+import com.observation.security.JWTAuthEntryPoint;
+import com.observation.security.JWTAuthenticationFilter;
+import com.observation.service.ServiceCustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,11 +26,18 @@ public class ConfigurationSecurity {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/auth").permitAll()
-                        .anyRequest().authenticated()
-                        //                .requestMatchers(HttpMethod.POST, "/user").hasAnyRole("ADMIN", "MODERATOR")
-                );
+                .cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(authEntryPoint).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/auth").permitAll()
+                //                .requestMatchers(HttpMethod.POST, "/user").hasAnyRole("ADMIN", "MODERATOR")
+                .anyRequest().authenticated();
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .requestMatchers(HttpMethod.POST, "/auth").permitAll()
+//                        .anyRequest().authenticated()
+//                        // .requestMatchers(HttpMethod.POST, "/user").hasAnyRole("ADMIN", "MODERATOR")
+//                );
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
