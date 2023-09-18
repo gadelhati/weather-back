8e96182cd89c678844fe6156a671f768f661a6b6
GadelhaTI <gadelha.ti@gmail.com>
2023-09-18 11:04:02 -0300

    enable method security: @PreAuthorize, @PosAuthorize, ...

--- src/main/java/com/observation/configuration/ConfigurationSecurity.java
+++ src/main/java/com/observation/configuration/ConfigurationSecurity.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,7 +21,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration @EnableWebSecurity @RequiredArgsConstructor
+@Configuration @EnableWebSecurity @EnableMethodSecurity
+@RequiredArgsConstructor
 public class ConfigurationSecurity {
 
     private final JWTAuthEntryPoint authEntryPoint;
